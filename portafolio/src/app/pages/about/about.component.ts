772395d3364ea674ce2feb40d6b883fadb8d0631
Jassiel Marcelino Ramirez Gutierrez <jassiel.ramirez@biaani.com>
2019-11-28 10:02:46 -0600

    Coneccion a la base de datos por medio de servicios y colocacion dinamica en el html de about

--- portafolio/src/app/pages/about/about.component.ts
+++ portafolio/src/app/pages/about/about.component.ts
@@ -1,3 +1,4 @@
+import { InfoPageService } from './../../services/info-page.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor( public infoService: InfoPageService ) { }
 
   ngOnInit() {
   }
